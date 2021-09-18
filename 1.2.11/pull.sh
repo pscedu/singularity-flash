e94d4fdea09588693ac6ed156baedbd18ec30fd8
icaoberg <icaoberg@andrew.cmu.edu>
2021-09-17 21:55:03 -0400

    Update pull.sh

--- 1.2.11/pull.sh
+++ 1.2.11/pull.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
 singularity pull https://depot.galaxyproject.org/singularity/flash%3A1.2.11--hed695b0_5
 mv -v flash%3A1.2.11--hed695b0_5 singularity-flash-1.2.11.sif
